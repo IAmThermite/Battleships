b37c8908fe12a29f2103e3848f146c464ce7e41a
LuciusDev <luke.honiss@gmail.com>
2016-09-08 21:42:23 +1200

    Added all the colours

--- src/BattleshipsMainFrame.java
+++ src/BattleshipsMainFrame.java
@@ -5,8 +5,17 @@ public class BattleshipsMainFrame {
     //setting up colours globally, they can be accessed by "BattleshipsMainFrame.<COLOUR_NAME>"
     public static final Color BG_COLOUR = new Color(205, 0, 35);
     public static final Color BUTTON_COLOUR = new Color(255, 205, 0);
+    
+    //colours for the panels
+    public static final Color HIT_COLOUR = new Color(245, 245, 90);
+    public static final Color MISS_COLOUR = Color.WHITE;
+    public static final Color NORMAL_COLOUR = new Color(0, 165, 165);
+    public static final Color HOVER_COLOUR = new Color(0, 125, 125);
+    public static final Color SHIP_COLOUR = Color.BLACK;
+    
     public static Font mainFont = new Font("Verdana", Font.PLAIN, 24);
     public static Font smallFont = new Font("Verdana", Font.PLAIN, 14);
+    public static Font mediumFont = new Font("Verdana", Font.PLAIN, 18);
     
     public static JFrame frame = new JFrame("Battleships!");
     
