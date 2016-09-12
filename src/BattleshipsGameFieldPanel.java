ffcb9b28d1400863b3141d1b64ce1c8e801fd89c
LuciusDev <luke.honiss@gmail.com>
2016-09-12 21:21:06 +1200

    changed some stuff
    font size, panel size, and centred text

--- src/BattleshipsGameFieldPanel.java
+++ src/BattleshipsGameFieldPanel.java
@@ -15,7 +15,7 @@ public class BattleshipsGameFieldPanel extends JPanel {
     
         setBackground(BattleshipsMainFrame.BG_COLOUR);
         setLayout(new GridLayout(11, 11));
-        setPreferredSize(new Dimension(350, 350));
+        setPreferredSize(new Dimension(300, 300));
         
         //add a blank panel
         JPanel blankPanel = new JPanel();
@@ -26,10 +26,10 @@ public class BattleshipsGameFieldPanel extends JPanel {
         for(int i = 0; i<10; i++) {
             String number = String.valueOf(i+1);
             
-            JLabel numLabel = new JLabel(number);
+            JLabel numLabel = new JLabel(number, JLabel.CENTER);
             numLabel.setBackground(BattleshipsMainFrame.BG_COLOUR);
             numLabel.setForeground(Color.WHITE);
-            numLabel.setFont(BattleshipsMainFrame.MAIN_FONT);
+            numLabel.setFont(BattleshipsMainFrame.MEDIUM_FONT);
             
             add(numLabel);
         }
@@ -38,10 +38,10 @@ public class BattleshipsGameFieldPanel extends JPanel {
         for(int i = 0; i<letters.length; i++) {
             String letter = letters[i];
             
-            JLabel letterLabel = new JLabel(letter);
+            JLabel letterLabel = new JLabel(letter, JLabel.CENTER);
             letterLabel.setBackground(BattleshipsMainFrame.BG_COLOUR);
             letterLabel.setForeground(Color.WHITE);
-            letterLabel.setFont(BattleshipsMainFrame.MAIN_FONT);
+            letterLabel.setFont(BattleshipsMainFrame.MEDIUM_FONT);
             
             add(letterLabel);
             
