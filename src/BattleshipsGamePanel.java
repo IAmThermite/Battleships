bc3446619b11e3e614564c92cee3c3109c0393de
LuciusDev <luke.honiss@gmail.com>
2016-09-12 21:24:11 +1200

    NETWORKING!
    Started doing the networking, it works for the most part. Also made the ships set be shown, will also process the opponents ships at the same time in BattleshipsSetupShips.java since the connection is open maybe?

--- src/BattleshipsGamePanel.java
+++ src/BattleshipsGamePanel.java
@@ -13,7 +13,6 @@ import java.util.*;
 public class BattleshipsGamePanel extends JPanel implements ActionListener {
     private JPanel opponentPanel;
     private JPanel playerPanel;
-    private JPanel logPanel;
     private JPanel controlPanel;
     
     private JLabel opponentLabel;
@@ -30,8 +29,29 @@ public class BattleshipsGamePanel extends JPanel implements ActionListener {
     public static ArrayList<BattleshipsGameMainPanel> listOfOpponentPanels = new ArrayList<BattleshipsGameMainPanel>(); //stores the instances of the opponents panels
     public static ArrayList<BattleshipsGameMainPanel> listOfPlayerPanels = new ArrayList<BattleshipsGameMainPanel>(); //stores the instances of the opponents panels
     
+    //stores the opponents ships
+    private ArrayList<BattleshipsGameMainPanel> opponentAircraftPanels = new ArrayList<BattleshipsGameMainPanel>();
+    private ArrayList<BattleshipsGameMainPanel> opponentBattleshipPanels = new ArrayList<BattleshipsGameMainPanel>();
+    private ArrayList<BattleshipsGameMainPanel> opponentSubmarinePanels = new ArrayList<BattleshipsGameMainPanel>();
+    private ArrayList<BattleshipsGameMainPanel> opponentDestroyerPanels = new ArrayList<BattleshipsGameMainPanel>();
+    private ArrayList<BattleshipsGameMainPanel> opponentMinesweeperPanels = new ArrayList<BattleshipsGameMainPanel>();
+    
+    //stores the players ships
+    private ArrayList<BattleshipsGameMainPanel> playerAircraftPanels = new ArrayList<BattleshipsGameMainPanel>();
+    private ArrayList<BattleshipsGameMainPanel> playerBattleshipPanels = new ArrayList<BattleshipsGameMainPanel>();
+    private ArrayList<BattleshipsGameMainPanel> playerSubmarinePanels = new ArrayList<BattleshipsGameMainPanel>();
+    private ArrayList<BattleshipsGameMainPanel> playerDestroyerPanels = new ArrayList<BattleshipsGameMainPanel>();
+    private ArrayList<BattleshipsGameMainPanel> playerMinesweeperPanels = new ArrayList<BattleshipsGameMainPanel>();
+    
+    //stuff for networking
+    private BufferedReader in;
+    private Socket connection;
+    
+    private BattleshipsCommand battleshipsCommandObject;
     
     public BattleshipsGamePanel() {
+        this.connection = BattleshipsHostJoin.connection;
+        
         setBackground(BattleshipsMainFrame.BG_COLOUR);
         setLayout(new GridLayout(2, 2));
         
@@ -44,10 +64,10 @@ public class BattleshipsGamePanel extends JPanel implements ActionListener {
         playerPanel = new JPanel();
         playerPanel.setBackground(BattleshipsMainFrame.BG_COLOUR);
         
-        playerLabel = new JLabel("Your grid");
+        playerLabel = new JLabel(BattleshipsHostJoin.username + "\'s grid");
         playerLabel.setBackground(BattleshipsMainFrame.BG_COLOUR);
         playerLabel.setForeground(Color.WHITE);
-        playerLabel.setFont(BattleshipsMainFrame.MAIN_FONT);
+        playerLabel.setFont(BattleshipsMainFrame.MEDIUM_FONT);
         
         playerPanel.setBackground(BattleshipsMainFrame.BG_COLOUR);
         playerPanel.add(playerLabel);
@@ -61,10 +81,10 @@ public class BattleshipsGamePanel extends JPanel implements ActionListener {
         
         opponentPanel = new JPanel();
         
-        opponentLabel = new JLabel("Opponents grid");
+        opponentLabel = new JLabel("Opponent's grid");
         opponentLabel.setBackground(BattleshipsMainFrame.BG_COLOUR);
         opponentLabel.setForeground(Color.WHITE);
-        opponentLabel.setFont(BattleshipsMainFrame.MAIN_FONT);
+        opponentLabel.setFont(BattleshipsMainFrame.MEDIUM_FONT);
         
         opponentPanel.setBackground(BattleshipsMainFrame.BG_COLOUR);
         opponentPanel.add(opponentLabel);
@@ -72,11 +92,8 @@ public class BattleshipsGamePanel extends JPanel implements ActionListener {
         
         
         //
-        //log panel
+        //log
         //
-        logPanel = new JPanel();
-        logPanel.setBackground(BattleshipsMainFrame.BG_COLOUR);
-        
         battleLog = new JTextArea();
         battleLog.setFont(BattleshipsMainFrame.SMALL_FONT);
         battleLog.setEditable(false);
@@ -85,8 +102,6 @@ public class BattleshipsGamePanel extends JPanel implements ActionListener {
             JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
             JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED
         );
-        
-        logPanel.add(logScroll);
         
         
         //
@@ -130,26 +145,144 @@ public class BattleshipsGamePanel extends JPanel implements ActionListener {
         
         
         
-        //add all the panels
+        //add all the panels/log
         add(playerPanel);
         add(logScroll);
         add(opponentPanel);
         add(controlPanel);
+        
+        
+        
+        //setup stuff
+        setupPlayerShips();
+        
+        battleshipsCommandObject = new BattleshipsCommand();
+        
+        GetCommand commandObject = new GetCommand();
+        Thread t1 = new Thread(commandObject);
+        t1.start();
+    }
+    
+    private class GetCommand extends Thread {
+        //this subclass will continuously check for a new command and send it to the
+        private String command;
+        
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(connection.getInputStream()));;
+                while(true) {
+                    String line = in.readLine();
+                    if(line != null) {
+                        System.out.println(line);
+                    }
+                }
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        public String getCommand() {
+            return command;
+        }
+    }
+    
+    private void setupPlayerShips() {
+        //add the aircraft panels to list
+        for(int i = 0; i<BattleshipsSetupShips.aircraftLocations.size(); i++) {
+            for(int x = 0; x<listOfPlayerPanels.size(); x++) {
+                if(BattleshipsSetupShips.aircraftLocations.get(i).equals(listOfPlayerPanels.get(x).getName())) {
+                    playerAircraftPanels.add(listOfPlayerPanels.get(x));
+                }
+            }
+        }
+        
+        //add the battleship panels to list
+        for(int i = 0; i<BattleshipsSetupShips.battleshipLocations.size(); i++) {
+            for(int x = 0; x<listOfPlayerPanels.size(); x++) {
+                if(BattleshipsSetupShips.battleshipLocations.get(i).equals(listOfPlayerPanels.get(x).getName())) {
+                    playerBattleshipPanels.add(listOfPlayerPanels.get(x));
+                }
+            }
+        }
+        
+        //add the submarine panels to list
+        for(int i = 0; i<BattleshipsSetupShips.submarineLocations.size(); i++) {
+            for(int x = 0; x<listOfPlayerPanels.size(); x++) {
+                if(BattleshipsSetupShips.submarineLocations.get(i).equals(listOfPlayerPanels.get(x).getName())) {
+                    playerSubmarinePanels.add(listOfPlayerPanels.get(x));
+                }
+            }
+        }
+        
+        //add the destroyer panels to list
+        for(int i = 0; i<BattleshipsSetupShips.destroyerLocations.size(); i++) {
+            for(int x = 0; x<listOfPlayerPanels.size(); x++) {
+                if(BattleshipsSetupShips.destroyerLocations.get(i).equals(listOfPlayerPanels.get(x).getName())) {
+                    playerDestroyerPanels.add(listOfPlayerPanels.get(x));
+                }
+            }
+        }
+        
+        //add the minesweeper panels to list
+        for(int i = 0; i<BattleshipsSetupShips.minesweeperLocations.size(); i++) {
+            for(int x = 0; x<listOfPlayerPanels.size(); x++) {
+                if(BattleshipsSetupShips.minesweeperLocations.get(i).equals(listOfPlayerPanels.get(x).getName())) {
+                    playerMinesweeperPanels.add(listOfPlayerPanels.get(x));
+                }
+            }
+        }
+        
+        
+        //set all the ships on the board
+        for(int i = 0; i<playerAircraftPanels.size(); i++) {
+            playerAircraftPanels.get(i).setShip();
+        }
+        for(int i = 0; i<playerBattleshipPanels.size(); i++) {
+            playerBattleshipPanels.get(i).setShip();
+        }
+        for(int i = 0; i<playerSubmarinePanels.size(); i++) {
+            playerSubmarinePanels.get(i).setShip();
+        }
+        for(int i = 0; i<playerDestroyerPanels.size(); i++) {
+            playerDestroyerPanels.get(i).setShip();
+        }
+        for(int i = 0; i<playerMinesweeperPanels.size(); i++) {
+            playerMinesweeperPanels.get(i).setShip();
+        }
+    }
+    
+    //checks to see if there is a ship on player board and will set the panel colour
+    private void checkShip() {
+        
+    }
+    
+    //get the initial command and setup ships
+    private void setOpponentShips() {
+        
     }
     
     public void actionPerformed(ActionEvent e) {
-        String location = controlLocationLabel.getText(); 
+        String location = controlLocationLabel.getText();
         
         for(int i = 0; i <listOfOpponentPanels.size(); i++) {
             if(listOfOpponentPanels.get(i).getName().equals(location)) {
-                if(listOfOpponentPanels.get(i).getIsHit()) {
-                    JOptionPane.showMessageDialog(this, "You have already shot at that panel!", "Invalid Location!", JOptionPane.ERROR_MESSAGE);
-                } else {
-                    battleLog.append(location + " was shot at!\n");
-                    listOfOpponentPanels.get(i).setHit();
+                if(!listOfOpponentPanels.get(i).getIsPlayer()) { //if it isnt the player panel
+                    if(listOfOpponentPanels.get(i).getIsHit()) {
+                        JOptionPane.showMessageDialog(this, "You have already shot at that panel!", "Invalid Location!", JOptionPane.ERROR_MESSAGE);
+                    } else {
+                        battleLog.append(location + " was shot at!\n");
+                        battleshipsCommandObject.sendCommand(location); //send command
+                        listOfOpponentPanels.get(i).setHit();
+                        
+                        if(listOfOpponentPanels.get(i).getHasShip()) {
+                            battleLog.append("You hit one!\n");
+                        } else {
+                            battleLog.append("You missed!\n");
+                        }
+                    }
                 }
                 break; //for some reason it loops through twice so break out of it
             }
-        } 
+        }
     }
 }
